14746ebedc229af2a2b69e2f433498ee7c908867
Preston <pres.10.nap@gmail.com>
2019-09-24 11:40:44 -0400

    initial branch commit

---
+++ src/main/java/com/lambdaschool/spotifysongsuggester/spotify/search/interfaces/ISearchModelObject.java
@@ -0,0 +1,7 @@
+package com.lambdaschool.spotifysongsuggester.spotify.search.interfaces;
+
+import com.wrapper.spotify.model_objects.IModelObject;
+
+public interface ISearchModelObject extends IModelObject {
+
+}
